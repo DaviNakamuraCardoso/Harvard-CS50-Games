dafe0d541540e0455b7211a458d4dae797e2db6c
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-09-09 15:49:33 -0300

    Fifty Fighters 19
    1. Implementing buttons

---
+++ Projeto-Final/FiftyFighters19/backgrounds.lua
@@ -0,0 +1,36 @@
+--====================== The Background Table ================================--
+
+Backgrounds = {
+    ['Castle'] = {
+        ['width'] = 640,
+        ['height'] = 384
+    },
+    ['Gym'] = {
+        ['width'] = 752,
+        ['height'] = 360
+    },
+    ['Mansion'] = {
+        ['width'] = 768,
+        ['height'] = 368
+    },
+    ['Gas'] = {
+        ['width'] = 752,
+        ['height'] = 224
+    },
+    ['Korea'] = {
+        ['width'] = 752,
+        ['height'] = 224
+    },
+    ['Bay'] = {
+        ['width'] = 800,
+        ['height'] = 336
+    },
+    ['Forest'] = {
+        ['width'] = 800,
+        ['height'] = 336
+    },
+    ['Factory'] = {
+        ['width'] = 740,
+        ['height'] = 224
+    }
+}
