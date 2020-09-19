17cf41d273851f08701780cb6c0b9cf12b7837cf
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-09-18 21:59:28 -0300

    Fifty Fighters 42
    Allowing the character select

--- Projeto-Final/FiftyFighters26/Button.lua
+++ Projeto-Final/FiftyFighters26/Button.lua
@@ -19,20 +19,26 @@ function Button:init(params)
     self.label = params.label or nil
     self.action = params.action or nil
 
+    self.border = params.border or {6, 6}
+
     self.noHover = params.color or {1, 1, 0.1, 1}
     self.hover = {self.noHover[1] / 3, self.noHover[2] / 3, self.noHover[3] / 3, 1}
     self.color = self.noHover
 
     self.font = params.font or 'great_fighter.ttf'
+    self.size = params.size or 20
 
-    self.imageSource = params.image or nil
+    self.imageSource = params.imageSource or nil
 
-    if self.image ~= nil then
+    if self.imageSource ~= nil then
         self.image = love.graphics.newImage(self.imageSource)
         self.width = self.image:getWidth() + 6
         self.height = self.image:getHeight() + 6
         self.quad = love.graphics.newQuad(0, 0, self.image:getWidth(), self.image:getHeight(), self.image:getDimensions())
+
     end
+
+    self.noLabel = params.noLabel or false
 
     self.message = Message{
         text = self.label,
@@ -41,8 +47,9 @@ function Button:init(params)
         relativeX = self.relativeX - VIRTUAL_WIDTH / 2,
         relativeY = self.relativeY - self.height / 2,
         font = self.font,
-        size = 20
+        size = self.size
     }
+
 
 end
 
@@ -68,8 +75,10 @@ end
 
 function Button:render()
     love.graphics.setColor(self.color[1], self.color[2], self.color[3], 1)
-    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height, 6, 6, 6, 6)
-    self.message:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height, self.border[1], self.border[2])
+    if not self.noLabel then
+        self.message:render()
+    end
     love.graphics.setColor(1, 1, 1, 1)
     if self.image ~= nil then
         love.graphics.draw(self.image, self.quad, self.x + 3, self.y + 3)
