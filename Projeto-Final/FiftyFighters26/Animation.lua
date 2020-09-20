2368a41d5ac0fa1abca12b154b3cbb8b4440a2fa
DaviNakamuraCardoso <davi.nakamura01@gmail.com>
2020-09-19 22:31:11 -0300

    Update Animation
    Allowing the characters table to pass a table to the animations

--- Projeto-Final/FiftyFighters26/Animation.lua
+++ Projeto-Final/FiftyFighters26/Animation.lua
@@ -7,7 +7,11 @@ function Animation:init(player, state)
     self.player = player
     self.state = state
     self.table = Characters[self.player.name]['animations'][state]
-    self.animationsTable = generateAnimation(player, self.table[1])
+    self.animations = {}
+    for i=1, #self.table do
+        self.animations[i] = generateAnimation(player, self.table[i])
+    end
+    self.animationsTable = self.animations[math.random(#self.table)]
     self.frames = self.animationsTable.frames
     self.quads = self.animationsTable.quads
     self.interval = 0.05
@@ -21,17 +25,18 @@ function Animation:init(player, state)
 end
 
 function Animation:update(dt)
+
     if self.timer >= self.interval then
 
         self.changing = true
         self.timer = 0
-        self.currentFrame = (self.currentFrame + 1) % (#self.frames + 1)
-        if self.currentFrame == #self.frames then
+        if self.currentFrame == #self.frames-1 then
             self.ending = true
             self.currentFrame = 0
         else
             self.ending = false
         end
+        self.currentFrame = (self.currentFrame + 1) % (#self.frames)
     else
         self.ending = false
         self.changing = false
@@ -51,7 +56,7 @@ end
 
 function Animation:shuffle()
     if not self.shuffled and self.player.state ~= self.state  and #self.table > 1 then
-        self.animationsTable = generateAnimation(self.player, self.table[math.random(#self.table)])
+        self.animationsTable = self.animations[math.random(#self.animations)]
         self.frames = self.animationsTable.frames
         self.quads = self.animationsTable.quads
         self.currentFrame = 0
